5cc108190afc46f019b2f8971192735896518c7e
almaas <airii7238@gmail.com>
2025-05-11 19:58:40 +0530

    Success

--- web.py
+++ web.py
@@ -1,50 +1,74 @@
-from http.server import HTTPServer,BaseHTTPRequestHandler
-content='''
-<html lang="en">
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+content = '''
+<!DOCTYPE html>
+<html>
 <head>
-    <title>lap specs.</title>
+    <title>TCP/IP Layers and Protocols</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #1e1e1e;
+            color: #ffffff;
+            text-align: center;
+        }
+        h2 {
+            color: #DE3163;
+            margin-top: 30px;
+        }
+        table {
+            margin: 0 auto;
+            border-collapse: collapse;
+            width: 80%;
+        }
+        th, td {
+            border: 1px solid #ffffff;
+            padding: 12px 20px;
+            font-size: 18px;
+        }
+        th {
+            background-color: #DE3163;
+            color: #000000;
+        }
+    </style>
 </head>
 <body>
-
-     <table border="3" cellpadding="10">
-        <caption style="font-size: x-large;">LAPTOP SPECIFICATTION</caption>
-        <TR>
-            <TD>BRAND</TD>
-            <TD>LENOVO</TD>
-        </TR>
-        <TR>
-            <TD>SERIES</TD>
-            <TD>THINKPAD E16 GEN1</TD>
-        </TR>
-        <TR>
-            <TD>PROCESSOR BRAND</TD>
-            <TD>INTEL</TD>
-        </TR>
-        <TR>
-            <TD>PROCESSOR TYPE</TD>
-            <TD>CORE I5</TD>
-        </TR>
-        <TR>
-            <TD>GRAPHICS CARD INTERFACE</TD>
-            <TD>INTEGRATED</TD>
-        </TR>
-        <TR>
-            <TD>OPERATING SYSTEM</TD>
-            <TD>WINDOWS 11 HOME</TD>
-        </TR>
-     </table> 
+    <h2>TCP/IP Layers and Protocols</h2>
+    <table>
+        <tr>
+            <th>TCP/IP Layers</th>
+            <th>Protocols (Examples)</th>
+        </tr>
+        <tr>
+            <td>Application Layer</td>
+            <td>HTTP, RDP, DNS, SMTP, Telnet, SNMP</td>
+        </tr>
+        <tr>
+            <td>Transport Layer</td>
+            <td>TCP, UDP</td>
+        </tr>
+        <tr>
+            <td>Internet Layer</td>
+            <td>ICMP, IGMP, ARP, IP, IPSec</td>
+        </tr>
+        <tr>
+            <td>Network Access Layer</td>
+            <td>Ethernet (IEEE 802.3), Token Ring, PPP, Frame Relay</td>
+        </tr>
+    </table>
 </body>
 </html>
 '''
+
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("Get request received...")
+        print("GET request received...")
         self.send_response(200) 
-        self.send_header("content-type", "text/html")       
+        self.send_header("Content-Type", "text/html")       
         self.end_headers()
         self.wfile.write(content.encode())
 
 print("This is my webserver") 
-server_address =('',8000)
-httpd = HTTPServer(server_address,MyServer)
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, MyServer)
 httpd.serve_forever()
